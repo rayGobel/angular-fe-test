1806563460214967a907bcd6fa3b3afd5c5e0b5a
Raynal R. Gobel <raynal.gobel@gmail.com>
2022-10-18 09:10:32 +0700

    Add Registration controller

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { LoginCredential } from '../login-form/login-form.component';
 import { environment } from '../../environments/environment';
 
+interface RegistrationCredential {
+}
+
 interface UserSession {
   id: string;
   userId: string;
@@ -16,6 +19,13 @@ export class AuthenticationService {
   constructor(private http: HttpClient) { }
 
   login(param: LoginCredential) {
+    const baseApi = environment.baseApi;
+    const url = `${baseApi}/auth/email-login`;
+
+    return this.http.post<UserSession>(url, param);
+  }
+
+  register(param: RegistrationCredential) {
     const baseApi = environment.baseApi;
     const url = `${baseApi}/auth/email-login`;
 
